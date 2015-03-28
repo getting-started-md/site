fa8d8d035357486fca10b89be20a31ca546460d6
Justin McNally <justin@kohactive.com>
2015-03-27 23:10:31 -0500

    fix port

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 #!venv/bin/python 
+
+import os
 from getting_started import app
 
 if __name__ == '__main__':
   app.debug = True
-  app.run('0.0.0.0', 5000)
+  app.run('0.0.0.0',  os.getenv('PORT', 5000))

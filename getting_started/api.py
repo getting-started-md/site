d43656bd44123da5c5320835138f39fb4b31c06a
Justin McNally <justin@kohactive.com>
2015-03-29 11:34:49 -0500

    update api

--- getting_started/api.py
+++ getting_started/api.py
@@ -7,11 +7,15 @@ import os
 @app.route('/api/posts')
 def posts():
   posts = []
+  files = []
   for root, dirs, filenames in os.walk("getting_started/data"):
     for f in filenames:
-      markdown = frontmatter.loads(open(os.path.join(root, f),'r').read())
-      posts.append({"content": markdown.content, "metadata": markdown.metadata})
+      files.append(f)
+
+  files = sorted(files)
+
+  for f in files:
+    markdown = frontmatter.loads(open(os.path.join(root, f),'r').read())
+    posts.append({"content": markdown.content, "metadata": markdown.metadata})
+
   return jsonify({"posts": posts})
-
-
-  
